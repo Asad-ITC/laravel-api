2cb28d4f6f0010806cbad17894bc258aebbbd641
Asad <asad.sandbox2000@gmail.com>
2024-12-09 17:09:42 +0600

    List function called

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +8,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get("test", function () {
+Route::get('test', function () {
     return ["name"=> "test API", "dataType"=> "JSON"];
 });
+
+Route::get('students', [StudentController::class, 'list']);
