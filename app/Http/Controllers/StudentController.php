a93681d3dc1984c25f0b480f0ef99fbf2718fba1
Asad <asad.sandbox2000@gmail.com>
2024-12-09 17:14:30 +0600

    Make GET API with database

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
     //
     function list() {
-        return "List function called";
+        // return "List function called";
+        return Student::all();
     }
 }
